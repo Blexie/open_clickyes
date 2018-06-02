f2dd1d559617cd7fec61162e28347a7442c3c42f
Blexie <fraser.blexie@gmail.com>
2018-06-02 13:01:00 +0100

    Implemented systray icon for user friendliness.

--- open_clickyes.py
+++ open_clickyes.py
@@ -1,8 +1,44 @@
 import pywinauto
+import time
+from infi.systray import SysTrayIcon
+running = None
+if running:
+    icon="clickyes_running.ico"
+else:
+    icon="clickyes.ico"
 
-def autoaccept():
-    app = pywinauto.Application().connect(class_name="#32770")
-    dlg = app['Microsoft Outlook']
-    dlg.Allow.click()
 
-autoaccept()
+def start_clickyes(systray):
+    global running
+    if running:
+        print("Stopping Clickyes...")
+        running = False
+        pass
+    else:
+        print("Clickyes Running!")
+        running = True
+        clickyes()
+
+    
+
+def clean_exit(systray):
+    global running
+    running = False
+    raise SystemExit
+
+def clickyes():
+    while running:
+        time.sleep(1)
+        try:
+            app = pywinauto.Application().connect(title='Microsoft Outlook', class_name="#32770")
+            dlg = app['Microsoft Outlook']
+            dlg.Allow.click()
+        except (pywinauto.findbestmatch.MatchError, pywinauto.findwindows.ElementNotFoundError, pywinauto.base_wrapper.ElementNotEnabled):
+            pass
+
+
+
+
+menu_options = (("Start", None, start_clickyes),)
+systray = SysTrayIcon(icon, "Open ClickYes v0.1", menu_options, on_quit=clean_exit)
+systray.start()
