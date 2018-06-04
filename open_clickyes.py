0f8e2c82e2da64dcd3187a33d20bfe8eb826e439
Blexie <fraser.blexie@gmail.com>
2018-06-04 11:55:15 +0100

    Major revision, tray icon fully implemented and I learned what classes are.

--- open_clickyes.py
+++ open_clickyes.py
@@ -1,44 +1,44 @@
 import pywinauto
 import time
 from infi.systray import SysTrayIcon
-running = None
-if running:
-    icon="clickyes_running.ico"
-else:
-    icon="clickyes.ico"
 
 
-def start_clickyes(systray):
-    global running
-    if running:
-        print("Stopping Clickyes...")
-        running = False
-        pass
-    else:
-        print("Clickyes Running!")
-        running = True
-        clickyes()
+class SysTray:
+    icon = "clickyes.ico"
+    def start_clickyes(systray):
+        if ClickYes.running:
+            print("Stopping ClickYes.")
+            ClickYes.running = False
+            SysTray.update_systray_stopped()
+        else:
+            print("Starting ClickYes!")
+            ClickYes.running = True
+            SysTray.update_systray_running()
+            ClickYes.toggle()
 
-    
+    def clean_exit(systray):
+        raise SystemExit
 
-def clean_exit(systray):
-    global running
-    running = False
-    raise SystemExit
+    def update_systray_running():
+        SysTray.systray.update(icon = "clickyes_running.ico")
 
-def clickyes():
-    while running:
-        time.sleep(1)
-        try:
-            app = pywinauto.Application().connect(title='Microsoft Outlook', class_name="#32770")
-            dlg = app['Microsoft Outlook']
-            dlg.Allow.click()
-        except (pywinauto.findbestmatch.MatchError, pywinauto.findwindows.ElementNotFoundError, pywinauto.base_wrapper.ElementNotEnabled):
-            pass
+    def update_systray_stopped():
+        SysTray.systray.update(icon = "clickyes.ico")
+
+    menu_options = (("Toggle (double-click)", None, start_clickyes),)
+    systray = SysTrayIcon(icon, "Open ClickYes v0.1", menu_options, on_quit=clean_exit)
 
 
+class ClickYes:
+    running = ""
+    def toggle():
+        while ClickYes.running:
+            time.sleep(1)
+            try:
+                app = pywinauto.Application().connect(title='Microsoft Outlook', class_name="#32770")
+                dlg = app['Microsoft Outlook']
+                dlg.Allow.click()
+            except (pywinauto.findbestmatch.MatchError, pywinauto.findwindows.ElementNotFoundError, pywinauto.base_wrapper.ElementNotEnabled):
+                pass
 
-
-menu_options = (("Start", None, start_clickyes),)
-systray = SysTrayIcon(icon, "Open ClickYes v0.1", menu_options, on_quit=clean_exit)
-systray.start()
+SysTray.systray.start()
