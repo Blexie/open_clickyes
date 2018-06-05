72af5156d3ce01c2a1901787aed5dc0deae74ccf
Blexie <fraser.blexie@gmail.com>
2018-06-05 09:19:43 +0100

    Working, just. See comments.

--- open_clickyes.py
+++ open_clickyes.py
@@ -1,6 +1,7 @@
 import pywinauto
 import time
 from infi.systray import SysTrayIcon
+from ctypes import windll, Structure, c_long, byref
 
 
 class SysTray:
@@ -33,12 +34,29 @@ class ClickYes:
     running = ""
     def toggle():
         while ClickYes.running:
-            time.sleep(1)
             try:
-                app = pywinauto.Application().connect(title='Microsoft Outlook', class_name="#32770")
-                dlg = app['Microsoft Outlook']
-                dlg.Allow.click()
-            except (pywinauto.findbestmatch.MatchError, pywinauto.findwindows.ElementNotFoundError, pywinauto.base_wrapper.ElementNotEnabled):
+                app = pywinauto.Application().connect(title='Microsoft Outlook', class_name="#32770")['Microsoft Outlook']
+                app.Allow.Wait('ready', retry_interval=0.1)
+                app.Allow.Click()
+                #Gross Hack: See https://stackoverflow.com/questions/50682448/pywinauto-click-fails-until-mouse-is-physically-clicked
+                #Might be able to catch mouse clicks and stop it interfering with wherever (0, 0) happens to be?
+                pos = queryMousePosition()
+                pywinauto.mouse.release()
+                pywinauto.mouse.move(coords=pos)
+                #/Hack
                 pass
+
+
+            except (pywinauto.findbestmatch.MatchError, pywinauto.findwindows.ElementNotFoundError):
+                time.sleep(0.1)
+                pass
+
+class POINT(Structure):
+    _fields_ = [("x", c_long), ("y", c_long)]
+
+def queryMousePosition():
+    pt = POINT()
+    windll.user32.GetCursorPos(byref(pt))
+    return  pt.x, pt.y
 
 SysTray.systray.start()
